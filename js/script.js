c9b532d0fe73814a559c6e78053016f93a238dcb
E-McNeill <ericaksaunders@gmail.com>
2019-03-07 12:02:25 -0800

    Updated comments to be more readable

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 /******************************************
 Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
+project 1 - A Random Quote Generator - by Erica Saunders
 ******************************************/
 
-// Array of objects that are quotes from various The Simpsons episodes
+/* Array of objects that are quotes from various The Simpsons episodes.
+Each object contains the quote and source. Additional properties (year, citation) included on some objects. 
+All objects have an additional 'tags' property, categorizing the quote as coming from a main or secondary character.*/
 let quotes = [
   {quote:"I didn\'t do it. Nobody saw me do it. You can\'t prove anything.", source:"Bart Simpson", citation:"" , year:"February 11th 1990", tag: "main character"},
   {quote:"Maybe, just once, someone will call me \'Sir\' without adding, \'You\'re making a scene\’.", source:"Homer Simpson", citation:"The Simpsons, Season 7, Episode 14", year:"February 4th 1996", tag: "main character"},
@@ -13,42 +15,42 @@ let quotes = [
 ];
 
 
-// Function to calculate a random number and use it to pull a random quote from the 'quotes' array
+// Function to calculate a random number and use it to pull a random quote from the 'quotes' array.
   function getRandomQuote() {
   let randomNumber = quotes[Math.floor(Math.random()*quotes.length)];
   return randomNumber;
 }
 
-//Array of colours from The Simpsons cartoon colour palette 
+//Array of colours from The Simpsons cartoon colour palette. 
 let colours = [ "#3E7DC3", "#F6D519", "#EE592D", "#F7C6CC", "#4C545C" ];
 
-//Function to calculate a random number and use it to pull a colour from the 'colours' array
+//Function to calculate a random number and use it to pull a colour from the 'colours' array.
 function getRandomColour() {
   let randomColour = colours[Math.floor(Math.random()*colours.length)];
   return randomColour;
 }
 
-// Function to compile the elements of the 'quote' array and print them to the screen and calls the getRandomColour function to change the background colour
+// Function to compile the elements of the 'quote' array and print them to the screen and calls the getRandomColour function to change the background colour.
 function printQuote() {
   let quoteHolder = getRandomQuote();
-  let entireString = '';
-   entireString += '<p class="quote">' + quoteHolder.quote + '</p>' + '<p class="source">' + quoteHolder.source ;  
+  let html = '';
+    html += '<p class="quote">' + quoteHolder.quote + '</p>' + '<p class="source">' + quoteHolder.source ;  
         if (quoteHolder.citation ) {
-        entireString +='<span class="citation">' + quoteHolder.citation + '</span>'; 
+        html +='<span class="citation">' + quoteHolder.citation + '</span>'; 
         }
         if (quoteHolder.year) {
-        entireString += '<span class="year">' + quoteHolder.year + '</span>'; 
+        html += '<span class="year">' + quoteHolder.year + '</span>'; 
         }
         if (quoteHolder.tag) {
-        entireString += '<br>' + '<br>' +'<span class="tag">' + quoteHolder.tag + '</span>'; 
+        html += '<br>' + '<br>' +'<span class="tag">' + quoteHolder.tag + '</span>'; 
         }
-   entireString += '</p>';
+    html += '</p>';
     
-  document.getElementById('quote-box').innerHTML = entireString;
+  document.getElementById('quote-box').innerHTML = html;
   document.body.style.backgroundColor = getRandomColour();
 }
 
-//If button isn't clicked, page will refresh every 25 seconds
+//Refreshes the quote and background colour every 25 seconds. 
 
 let intervalID = window.setInterval(printQuote, 25000);
 
